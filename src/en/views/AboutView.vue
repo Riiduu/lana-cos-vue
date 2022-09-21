1bf702b23d896b8f938e91bb43a0e9fdec1e8152
Riiduu <100873979+Riiduu@users.noreply.github.com>
2022-09-21 12:42:56 +0300

    fixed email form design, but it doesnt work yet

--- src/en/views/AboutView.vue
+++ src/en/views/AboutView.vue
@@ -73,6 +73,7 @@ export default {
 <style lang="scss" scoped>
   #navbar {
     position: fixed;
+    
   }
   
   #menu-fs {
@@ -90,6 +91,9 @@ export default {
   }
 
   .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     justify-content: center;
     margin: 140px 5% 0 5%;
     text-align: center;
