8620c0f1a6b1b129c8bd3cea4cdbadc97cb71f8a
riiduu <jelinsksk@gmail.com>
2022-09-26 10:03:29 +0300

    removed unused lines

--- src/main.js
+++ src/main.js
@@ -1,18 +1,5 @@
 import { createApp } from 'vue'
-import Vue from 'vue'
 import App from '@/en/App.vue'
 import router from '@/en/router'
-import * as VueGoogleMaps from "vue2-google-maps" // Import package
 
 createApp(App).use(router).mount('#app')
-
-Vue.config.productionTip = false
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: "AIzaSyBzMX22NxjPGiKF2i8cjfZjcfgcG6eE3dk",
-    libraries: "places"
-  }
-});
-new Vue({
-    render: h => h(App),
-  }).$mount('#app')
